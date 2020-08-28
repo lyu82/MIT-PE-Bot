a55a444442a1833be9ae73f18a6bfe0feef0482f
lyu82 <67127602+lyu82@users.noreply.github.com>
2020-08-28 18:50:58 -0500

    Update script.sh
    altered to use stock grep

--- script.sh
+++ script.sh
@@ -23,7 +23,7 @@ if [ $(($currenttime+11)) -gt $targettime ];then sleep 0.001 ;else sleep 5 ;fi
 currenttime=$(date +%s)
 done
 
-keycode=$(curl -s 'https://eduapps.mit.edu/mitpe/student/registration/sectionList' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: ' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' -H 'Referer: https://eduapps.mit.edu/mitpe/student/registration/home' -H "$cookie" --compressed | rg $term | cut -d'=' -f 3-3 | cut -d'"' -f1)
+keycode=$(curl -s 'https://eduapps.mit.edu/mitpe/student/registration/sectionList' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: ' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' -H 'Referer: https://eduapps.mit.edu/mitpe/student/registration/home' -H "$cookie" --compressed | grep $term | cut -d'=' -f 3-3 | cut -d'"' -f1)
 
 curl -s 'https://eduapps.mit.edu/mitpe/student/registration/create' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' -H 'Origin: https://eduapps.mit.edu' -H 'Upgrade-Insecure-Requests: 1' -H 'Content-Type: application/x-www-form-urlencoded' -H 'User-Agent: ' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3' -H "$cookie" --data "sectionId=$keycode&mitId=$id&wf=%2Fregistration%2Fquick" --compressed
 echo $keycode
